f4e0d0216c5a20df0e7682be91200e318efbb83c
IvanOoi <ivanooichenliang@gmail.com>
2022-01-16 18:22:56 +0800

    Add scanner to main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        System.out.println(x);
     }
 }
